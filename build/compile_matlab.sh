3bec4635d773cd90f244272f752a2780d25cd887
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2024-01-25 13:28:57 -0600

    paths

--- build/compile_matlab.sh
+++ build/compile_matlab.sh
@@ -13,9 +13,9 @@ export PATH=${MATLABROOT}/bin:${PATH}
 # We use SPM12's standalone tool, but edited to add our own code to the 
 # compilation path
 matlab -nodisplay -nodesktop -nosplash -sd "${WD}" -r \
-    "spm_make_standalone_local('${SPM_PATH}','${WD}/bin','${WD}/src'); exit"
+    "spm_make_standalone_local('${SPM_PATH}','${WD}/../bin','${WD}/../src'); exit"
 
 # We grant lenient execute permissions to the matlab executable and runscript so
 # we don't have hiccups later.
-chmod go+rx "${WD}"/bin/spm12
-chmod go+rx "${WD}"/bin/run_spm12.sh
+chmod go+rx "${WD}"/../bin/spm12
+chmod go+rx "${WD}"/../bin/run_spm12.sh
